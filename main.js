9e43b750b510ac8904a69bcc0e90d3bbcd614b0d
ceciliasegura <ceciliasegura.t@hotmail.com>
2023-02-02 23:10:13 +0100

    Added code

---
+++ main.js
@@ -0,0 +1,16 @@
+const circuloContainer = document.querySelector(".circle-container")
+const content = document.querySelector(".content")
+
+
+
+function spin() {
+    circuloContainer.classList.toggle("rotate")
+    content.classList.toggle("turn")
+
+}
+
+
+
+
+
+
